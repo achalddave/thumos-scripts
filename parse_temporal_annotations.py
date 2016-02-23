3ca06dcd05469c9440441f013c98383c42e7fc5b
Achal Dave <achalddave@live.com>
2016-02-23 17:12:19 -0500

    Add script to parse temporal annotations, dump JSON

---
+++ parse_temporal_annotations.py
@@ -0,0 +1,81 @@
+"""Parse temporal annotations for THUMOS '14 validation data, output JSON.
+
+Output format:
+    [
+        {
+            filename: ...,
+            start_seconds: ...,
+            end_seconds: ...,
+            start_frame: ...,
+            end_frame: ...,
+            frames_per_second: ...,
+            category: ...
+        },
+        ...
+    ]
+"""
+
+import argparse
+import csv
+import json
+import os
+from math import ceil, floor
+from os import path
+
+parser = argparse.ArgumentParser(
+    description=__doc__,
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('input_annotation_dir')
+parser.add_argument('video_frames_info',
+                    help='CSV of format <video_name>,<fps>[,<num_frames_in_video>]?')
+parser.add_argument('output_annotation_json')
+
+args = parser.parse_args()
+
+def parse_frame_info(video_frames_info_path):
+    video_fps = dict()
+    with open(video_frames_info_path) as f:
+        reader = csv.reader(f)
+        next(reader, None) # Skip headers
+        for row in reader:
+            video_fps[row[0]] = float(row[1])
+    return video_fps
+
+def parse_annotation_file(annotation_path, video_fps):
+    annotations = []
+    with open(annotation_path) as f:
+        for line in f:
+            # Format: "<video_name>  <start_time> <end_time>" Note that there
+            # are *two spaces* between the first two fields (unfortunately).
+            [filename, _, start, end] = line.strip().split(' ')
+            start, end = float(start), float(end)
+            current_fps = video_fps[filename]
+            start_frame = floor(start * current_fps)
+            end_frame = ceil(end * current_fps)
+            annotations.append({'filename': filename,
+                                'start_seconds': start,
+                                'end_seconds': end,
+                                'start_frame': start_frame,
+                                'end_frame': end_frame,
+                                'frames_per_second': current_fps
+                                })
+    return annotations
+
+def main():
+    annotation_paths = ["%s/%s" % (args.input_annotation_dir, x)
+                        for x in os.listdir(args.input_annotation_dir)]
+    # Maps video name to frames per second
+    video_fps = parse_frame_info(args.video_frames_info)
+    annotations = []
+    for annotation_path in annotation_paths:
+        # annotation_path is of the form /path/to/[category]_val.txt.
+        category = path.basename(annotation_path)[:-len("_val.txt")]
+        annotation_details = parse_annotation_file(annotation_path, video_fps)
+        for annotation in annotation_details:
+            annotation['category'] = category
+        annotations.extend(annotation_details)
+    with open(args.output_annotation_json, 'wb') as f:
+        json.dump(annotations, f)
+
+if __name__ == '__main__':
+    main()
